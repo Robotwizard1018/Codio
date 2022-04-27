94cd0f69ba6351d932b4994412ad2870ca64015c
Christopher Gill <micaigill1018@gmail.com>
2022-04-27 17:41:54 +0000

    This is my first commit to cypher.py

---
+++ cypher.py
@@ -0,0 +1,29 @@
+#!/bin/bash
+import sys
+
+
+
+def cypher(sentence:str, n:int) -> str:
+    #Uppercase letters 65-90
+    #Lowercase letters 97-122
+    sentence = sentence.upper()
+    results = ""
+
+    for letter in sentence:
+        #Converts character to its numerical value
+        new_letter = ord(letter)
+        
+        #Checks if characters numerical value is uppercase     
+        if new_letter >= 65 and new_letter <=90:
+            new_letter += int(n)
+            if new_letter > 90:
+                new_letter = abs(new_letter - 26)
+
+        #Reverts character back from its numerical value 
+        results += chr(new_letter)
+        
+    return results
+print("Enter a number to bump from followed by a sentence in a string form: ")
+n = sys.argv[1]
+sentence = sys.argv[2]
+print(cypher(sentence, n))
